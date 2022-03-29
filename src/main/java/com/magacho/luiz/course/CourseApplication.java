60a4aab733c9a19300f6efac11ac9caa0730fc4b
Luiz Fernando Magacho <luizmagacho94@gmail.com>
2022-03-29 18:18:46 -0300

    Add files via upload

---
+++ src/main/java/com/magacho/luiz/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.magacho.luiz.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
